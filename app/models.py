8582c90386184a56724744dace3c93155473aee5
Andrew Zaiko <andrewzaiko@gmail.com>
2022-09-25 15:11:46 +0300

    Добавил функцию get_hash

--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
+import base64
+import hashlib
+
 from app.create_db import db
+from constants import PWD_HASH_SALT, PWD_HASH_ITERATIONS
 
 
 # Создаём класс сущностей "Режиссёр" базы данных
@@ -37,3 +41,13 @@ class User(db.Model):
     username = db.Column(db.String)
     password = db.Column(db.String)
     role = db.Column(db.String)
+
+
+def get_hash(password):
+    hash_digest = hashlib.pbkdf2_hmac(
+        'sha256',
+        password.encode('utf-8'),  # Convert the password to bytes
+        PWD_HASH_SALT,
+        PWD_HASH_ITERATIONS
+    )
+    return base64.b64encode(hash_digest)
